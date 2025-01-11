354521ff9b3d53ab7fd05f9552efef7a65d2e4ad
Frajo Methippara <francisjohn097@gmail.com>
2025-01-12 00:48:23 +0100

    Wifi connected.

--- main/iot-app-esp32c3.cpp
+++ main/iot-app-esp32c3.cpp
@@ -10,41 +10,67 @@ extern "C" {
 
     #include "nvs_flash.h"
     #include "esp_log.h"
-}
 
-class HelloWorld {
-public:
-    void say_hello() {
-        std::cout << "Hello, World from C++!" << std::endl;
-        
-    }
-};
+    
+}
 
 #include "wifi.h"
 
 #include "esp_log.h"
 static const char* TAG = "WiFi";
 
-extern "C" void app_main() {
-    HelloWorld hello;
-    hello.say_hello();
+static EventGroupHandle_t wifi_event_group;
+const int WIFI_CONNECTED_BIT = BIT0; // Define the connection bit flag
 
-    //Initialize NVS
-    // esp_err_t ret_res = nvs_flash_init();
-    // if( ESP_ERR_NVS_NO_FREE_PAGES == ret_res || ESP_ERR_NVS_NEW_VERSION_FOUND == ret_res )
-    // {
-    //     ESP_ERROR_CHECK(nvs_flash_erase())
-    //     ret_res = nvs_flash_init();
-    // }
+extern "C" void ip_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+    if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+
+        // ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        // ESP_LOGI(TAG, "Connected! IP Address: %s", esp_ip4addr_ntoa(&event->ip_info.ip));
+        // xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
+
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+
+        char ip_str[16]; // Buffer to hold the IP address as a string
+        esp_ip4addr_ntoa(&event->ip_info.ip, ip_str, sizeof(ip_str));
+
+        ESP_LOGI(TAG, "Connected! IP Address: %s", ip_str);
+        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
+        std::cout << "IP address printed from ip instance"<< std::endl;
+    }
+}
+
+extern "C" void app_main() {
 
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_LOGI(TAG, "NVS initialized");
 
+    /* Setting up wifi */
+    ESP_LOGI(TAG, "Initializing Wi-Fi...");
+    wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
+        (esp_event_handler_t)wifi::getInstance()->getWifiHandlerAddress(), NULL, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &ip_event_handler, NULL, NULL));
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    
+    esp_netif_create_default_wifi_sta();
+
     wifi::getInstance()->init();
-    wifi::getInstance()->connect("pixel 4a", "123456789");
+    wifi::getInstance()->connect("Pixel 4a", "123456789");
+
+    ESP_LOGI(TAG, "Waiting for Wi-Fi connection...");
+    xEventGroupWaitBits(wifi_event_group, WIFI_CONNECTED_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+    ESP_LOGI(TAG, "Wi-Fi connected successfully!");
+
 
     // Example: Pause the program to allow observation
     while (true) {
+
+        std::cout<<"End of program"<<std::endl;
+        
         vTaskDelay(pdMS_TO_TICKS(1000)); // Wait for 1 second
     }
 }
--- main/wifi.cpp
+++ main/wifi.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 
-
 extern "C" {
-    // #include "freertos/FreeRTOS.h"
-    // #include "freertos/task.h"
-    // #include "esp_system.h"
-
     #include "esp_wifi.h"
-    #include "esp_event.h"
+    
 
     #include "nvs_flash.h"
     #include "esp_log.h"
@@ -15,44 +10,20 @@ extern "C" {
 
 #include "wifi.h"
 
-static EventGroupHandle_t wifi_event_group;
-const int WIFI_CONNECTED_BIT = BIT0; // Define the connection bit flag
-static const char* TAG = "WiFi";
+static const char* WIFI_TAG = "WiFi";
 
-#define WIFI_SSID      "Pixel 4a"
-#define WIFI_PASSWORD  "123456789"
-
-// Event handler for Wi-Fi events
-extern "C" void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+void wifi::wifiEventHandler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
         esp_wifi_connect();
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        ESP_LOGI(TAG, "Disconnected from Wi-Fi, retrying...");
+        ESP_LOGI(WIFI_TAG, "Disconnected from Wi-Fi, retrying...");
         esp_wifi_connect();
-    }
-}
-
-extern "C" void ip_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
-    if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-
-        // ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        // ESP_LOGI(TAG, "Connected! IP Address: %s", esp_ip4addr_ntoa(&event->ip_info.ip));
-        // xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
-
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-
-        char ip_str[16]; // Buffer to hold the IP address as a string
-        esp_ip4addr_ntoa(&event->ip_info.ip, ip_str, sizeof(ip_str));
-
-        ESP_LOGI(TAG, "Connected! IP Address: %s", ip_str);
-        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
-        std::cout << "IP address printed from ip instance"<< std::endl;
     }
 }
 
 wifi::wifi()
 {
-    std::cout<<"Singleton constructor is called and the class is invoked"<<std::endl;
 }
 
 wifi* wifi::getInstance()
@@ -61,83 +32,35 @@ wifi* wifi::getInstance()
     return &singleton_wifi_ins;
 }
 
-bool wifi::init()
+bool wifi::init( )
 {
-    // ESP_ERROR_CHECK(nvs_flash_init());
-    // ESP_LOGI(TAG, "NVS initialized");
-
-    /* Setting up wifi */
-    ESP_LOGI(TAG, "Initializing Wi-Fi...");
-    wifi_event_group = xEventGroupCreate();
-
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
-
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &ip_event_handler, NULL, NULL));
-
-    wifi_config_t wifi_config = {};
-    std::strncpy((char*)wifi_config.sta.ssid, WIFI_SSID, sizeof(wifi_config.sta.ssid));
-    std::strncpy((char*)wifi_config.sta.password, WIFI_PASSWORD, sizeof(wifi_config.sta.password));
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_LOGI(TAG, "Waiting for Wi-Fi connection...");
-    xEventGroupWaitBits(wifi_event_group, WIFI_CONNECTED_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
-    ESP_LOGI(TAG, "Wi-Fi connected successfully!");
 
     return true;
 }
 
 bool wifi::connect(char *wifi_ssid, char *wifi_pass)
 {    
-    // ESP_ERROR_CHECK(nvs_flash_init());
-    // ESP_LOGI(TAG, "NVS initialized");
+    wifi_config_t wifi_config = {};
 
-    // /* Setting up wifi */
-    // ESP_LOGI(TAG, "Initializing Wi-Fi...");
-    // wifi_event_group = xEventGroupCreate();
+    std::strncpy((char*)wifi_config.sta.ssid, wifi_ssid, sizeof(wifi_config.sta.ssid) - 1);
+    std::strncpy((char*)wifi_config.sta.password, wifi_pass, sizeof(wifi_config.sta.password) - 1);
 
-    // ESP_ERROR_CHECK(esp_netif_init());
-    // ESP_ERROR_CHECK(esp_event_loop_create_default());
-    // esp_netif_create_default_wifi_sta();
-
-    // wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    // ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    // ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
-    // ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL, NULL));
-
-    // wifi_config_t wifi_config = {};
-    // std::strncpy((char*)wifi_config.sta.ssid, WIFI_SSID, sizeof(wifi_config.sta.ssid));
-    // std::strncpy((char*)wifi_config.sta.password, WIFI_PASSWORD, sizeof(wifi_config.sta.password));
-
-    // ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    // ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-    // ESP_ERROR_CHECK(esp_wifi_start());
-
-    // ESP_LOGI(TAG, "Waiting for Wi-Fi connection...");
-    // xEventGroupWaitBits(wifi_event_group, WIFI_CONNECTED_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
-    // ESP_LOGI(TAG, "Wi-Fi connected successfully!");
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
 
     return true;
+}
+
+void* wifi::getWifiHandlerAddress()
+{
+    printf("Wifi Handler Address in class:%p\n", (void*)(&wifi::wifiEventHandler));
+    return (void*)(&wifi::wifiEventHandler);
 }
 
 bool wifi::disconnect()
 {
     return true;
 }
-
-void wifi::print()
-{
-    std::cout << "Hello. After Constructor is called, print function is invoked!!!!!!"<< std::endl;
-}
-
-
-
